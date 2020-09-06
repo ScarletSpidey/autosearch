cfee76f7274e4ec49fbb080867d47b50fccd588d
Shobit Puri <shobitpuri2@gmail.com>
2020-09-06 14:21:44 +0530

    added axios for fetching

--- autosearch/src/App.js
+++ autosearch/src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Search from './components/Search'
-import DummyData from './data/dummydata'
+//import DummyData from './data/dummydata'
+import axios from 'axios';
+
 
 function App () {
 
+  // const suggestions = [];
+  // for (var i=0;i<DummyData["data"].length;i++){
+  //   suggestions.push(DummyData["data"][i]["employee_name"]);
+  // }
+
   const suggestions = [];
-  for (var i=0;i<DummyData["data"].length;i++){
-    suggestions.push(DummyData["data"][i]["employee_name"]);    
-  }
+
+  axios.get('http://dummy.restapiexample.com/api/v1/employees')
+  .then(
+    res => {
+      console.log(res.data["data"]);
+      for (var i=0;i<res.data["data"].length;i++) {
+        suggestions.push(res.data["data"][i]["employee_name"]);
+      }
+    }
+  )
 
 
   return(
